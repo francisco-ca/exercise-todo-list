de9aabdbc13b0e628c84c3759cf762b4df81445f
francisco-ca <francisco4_1996@hotmail.com>
2020-10-03 18:54:51 +0000

    todolist terminado

---
+++ todoslist/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { render } from 'react-dom';
+import ControlledInput from "./Componentes/ControlledInput.js";
+
+
+function App() {
+  return (
+    <div className="App">
+        
+     <ControlledInput></ControlledInput>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ todoslist/src/Componentes/ControlledInput.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { BsX } from "react-icons/bs";
+
+const ControlledInput = () => {
+    const [task, setTask] = useState("");
+    const [list, setList] = useState([]);
+    const [complete, setComplete] = useState("none")
+    const deleteTask = i => {
+        
+        list.splice(i, 1);
+        
+        setList([...list])
+        
+    }
+    const largolista = list.length
+
+    
+
+    return (
+        <div>
+
+            <div className="container">
+                <h1 className="title"> todos</h1>
+                <form className="inputedit" onSubmit={(evento) => {
+                    evento.preventDefault();
+                    setList(list.concat(task));
+                    evento.target.task.value="";
+                   
+                }}>
+
+                    <input  placeholder="New task"
+                        name="task"
+                        type="text"
+                        onChange={(evento) => 
+                        setTask(evento.target.value)
+                        
+                        }>
+
+                              </input>
+                </form>
+                <ul>
+                    
+                   {list.length>0? 
+                   list.map(function(task, i){
+                    return <div class="border rounded"><li key={i}>{task}
+                    <button class="cerrar" 
+                    onClick={() =>deleteTask(i) }><BsX></BsX></button></li></div>})
+                    
+                    :<p class="empty">Empty list</p>}
+                    
+                </ul>
+                <div class="larglist"> {largolista} Item left</div>
+
+    
+
+
+        </div>
+</div>
+
+
+
+
+
+
+
+
+)
+}
+
+
+export default ControlledInput;
+
+/* <button onClick={() => this.removeItemFromArr(this.miarreglo,this.miarreglo[]) } ></button> */
